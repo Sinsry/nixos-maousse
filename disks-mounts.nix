bc2168c32c5cfd58f1fe27e3ae1c3b902a7a3551
Sinsry <Sinsry@users.noreply.github.com>
2026-01-13 20:44:35 +0100

    Réinitialisation du projet

---
+++ disks-mounts.nix
@@ -0,0 +1,67 @@
+{ _config, pkgs, ... }:
+{
+  # Support des systèmes de fichiers
+  boot.supportedFilesystems = [
+    "ntfs"
+    "exfat"
+    "vfat"
+    "ext4"
+    "btrfs"
+  ];
+
+  # Packages nécessaires
+  environment.systemPackages = with pkgs; [
+    ntfs3g
+    exfatprogs
+  ];
+
+  # Configuration des montages internes dans /mnt/
+  fileSystems."/mnt/Ventoy" = {
+    device = "/dev/disk/by-uuid/4E21-0000";
+    fsType = "exfat";
+    options = [
+      "nofail"
+      "rw"
+      "umask=0000"
+      "uid=1000"
+      "gid=100"
+    ];
+  };
+
+  # Configuration des montages internes dans /mnt/
+  fileSystems."/mnt/Windows" = {
+    device = "/dev/disk/by-uuid/5A0820A008207D5F";
+    fsType = "ntfs";
+    options = [
+      "nofail"
+      "noperm"
+      ];
+  };
+
+  # Configuration des montages internes dans /mnt/
+  fileSystems."/mnt/DataWindows" = {
+    device = "/dev/disk/by-uuid/363A21FE3A21BBAD";
+    fsType = "ntfs";
+    options = [
+      "nofail"
+      "noperm"
+    ];
+  };
+
+  # Configuration des montages internes dans /mnt/
+  fileSystems."/home/sinsry/Jeux" = {
+    device = "/dev/disk/by-uuid/c5f40b61-d064-468c-932a-c3460bc762ed";
+    fsType = "ext4";
+    options = [
+      "nofail"
+      ];
+  };
+
+  # Crée les points de montage
+  systemd.tmpfiles.rules = [
+    "d /mnt/Ventoy 0755 root root -"
+    "d /mnt/Windows 0755 root root -"
+    "d /mnt/DataWindows 0755 root root -"
+    "d /home/sinsry/Jeux 0755 root root -"
+    ];
+}
---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "Configuration NixOS de maousse";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    plasma-manager = {
+      url = "github:pjones/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+  };
+};
+  outputs = { self, nixpkgs, home-manager, plasma-manager }: {
+    nixosConfigurations.maousse = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.sinsry = {
+
+            imports = [ plasma-manager.homeModules.plasma-manager ];
+
+            home.stateVersion = "25.11";
+
+            programs.plasma = {
+              enable = true;
+              input.keyboard.numlockOnStartup = "on";
+              workspace = {
+                lookAndFeel = "org.kde.breezedark.desktop";
+                colorScheme = "BreezeDark";
+                theme = "breeze-dark";
+                iconTheme = "Papirus-Dark";
+                cursor.theme = "breeze_cursors";
+              };
+              # Désactiver le retour de lancement
+              configFile = {
+                "klaunchrc"."BusyCursorSettings"."Bouncing" = false;
+                "klaunchrc"."FeedbackStyle"."BusyCursor" = false;
+              };
+              };
+            };
+          }
+        ];
+     };
+   };
+}
