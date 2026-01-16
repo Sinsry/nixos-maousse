46fe71ae0b38340c1d86d878e9695d7d92f1aa59
Sinsry <Sinsry@users.noreply.github.com>
2026-01-16 20:57:55 +0100

    Update

--- configuration.nix
+++ configuration.nix
@@ -21,6 +21,11 @@
       "rd.systemd.show_status=false"
       "rd.udev.log_level=3"
       "udev.log_priority=3"
+
+    ];
+
+    kernelModules = [
+      "ntsync"
     ];
 
     supportedFilesystems = [
