d8ae5e8c2304285c25f06d380ab12ca9006d22cf
Sinsry <Sinsry@users.noreply.github.com>
2026-01-25 19:12:40 +0100

    Update

--- configuration.nix
+++ configuration.nix
@@ -360,7 +360,7 @@
 
 
   environment.shellAliases = {
-    nixrebuild = ''cd /etc/nixos && sudo git add . && sudo git commit -m 'Update' && sudo git push && cd ~/ && sudo nixos-rebuild switch --flake path:/etc/nixos#maousse'';
+    nixrebuild = ''sudo nixos-rebuild switch --flake path:/etc/nixos#maousse && cd /etc/nixos && sudo git add . && sudo git commit -m 'Update' && sudo git push && cd ~/ '';
     nixpush = "cd /etc/nixos && sudo git add . && sudo git commit -m 'Update' && sudo git push && cd ~/";
     nixlistenv = "sudo nix-env --list-generations --profile /nix/var/nix/profiles/system";
 
