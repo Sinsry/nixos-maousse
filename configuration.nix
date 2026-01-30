27b65a23447783d0f1b298c4a9a7a10c9f7cf2d2
Sinsry <Sinsry@users.noreply.github.com>
2026-01-30 18:58:14 +0100

    Update

--- configuration.nix
+++ configuration.nix
@@ -213,6 +213,7 @@
     vorta
     vulkan-tools
     wowup-cf
+    zed-editor
     (pkgs.writeTextDir "share/sddm/themes/breeze/theme.conf.user" ''
       [General]
       background=/etc/nixos/asset/wallpaper-sddm.png
