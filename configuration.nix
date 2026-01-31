5fb5bd519f038f3aafa9b65d3c3a1f057c27d82a
Sinsry <Sinsry@users.noreply.github.com>
2026-01-31 02:27:07 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -189,6 +189,14 @@
     git
     google-chrome
     goverlay
+    gstreamer
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-ugly
+    gst_all_1.gst-libav
+    gst_all_1.gst-vaapi
     kdePackages.breeze-gtk
     kdePackages.filelight
     kdePackages.kate
