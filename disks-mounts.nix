1f13f481add8a9ac8e3b38cc9a238f905bfc51f1
Sinsry <Sinsry@users.noreply.github.com>
2026-01-31 18:58:00 +0100

    Update

--- disks-mounts.nix
+++ disks-mounts.nix
@@ -1,36 +1,48 @@
 {
-  fileSystems."/mnt/Ventoy" = {
-    device = "/dev/disk/by-uuid/4E21-0000";
-    fsType = "exfat";
-    options = [
-      "nofail"
-      "rw"
-      "umask=0000"
-      "uid=1000"
-      "gid=100"
-    ];
+  #==== Montages disques ====
+  fileSystems = {
+    # Clé USB Ventoy
+    "/mnt/Ventoy" = {
+      device = "/dev/disk/by-uuid/4E21-0000";
+      fsType = "exfat";
+      options = [
+        "nofail"
+        "rw"
+        "umask=0000"
+        "uid=1000"
+        "gid=100"
+      ];
+    };
+
+    # Partition Windows
+    "/mnt/Windows" = {
+      device = "/dev/disk/by-uuid/90B4C5E3B4C5CBC2";
+      fsType = "ntfs";
+      options = [
+        "nofail"
+        "noperm"
+      ];
+    };
+
+    # Partition Data Windows
+    "/mnt/Data_Windows" = {
+      device = "/dev/disk/by-uuid/D8CA1469CA1445E2";
+      fsType = "ntfs";
+      options = [
+        "nofail"
+        "noperm"
+      ];
+    };
+
+    # Partition Jeux (ext4)
+    "/home/sinsry/Jeux" = {
+      device = "/dev/disk/by-uuid/c5f40b61-d064-468c-932a-c3460bc762ed";
+      fsType = "ext4";
+      options = [ "nofail" ];
+    };
   };
-  fileSystems."/mnt/Windows" = {
-    device = "/dev/disk/by-uuid/90B4C5E3B4C5CBC2";
-    fsType = "ntfs";
-    options = [
-      "nofail"
-      "noperm"
-    ];
-  };
-  fileSystems."/mnt/Data_Windows" = {
-    device = "/dev/disk/by-uuid/D8CA1469CA1445E2";
-    fsType = "ntfs";
-    options = [
-      "nofail"
-      "noperm"
-    ];
-  };
-  fileSystems."/home/sinsry/Jeux" = {
-    device = "/dev/disk/by-uuid/c5f40b61-d064-468c-932a-c3460bc762ed";
-    fsType = "ext4";
-    options = [ "nofail" ];
-  };
+
+  #==== Création des points de montage ====
   systemd.tmpfiles.rules = [
     "d /mnt/Ventoy 0755 root root -"
     "d /mnt/Windows 0755 root root -"
--- network-mounts.nix
+++ network-mounts.nix
@@ -1,34 +1,42 @@
 {
+  #==== Création des points de montage ====
   systemd.tmpfiles.rules = [
     "d /mnt/Data 0755 root root -"
     "d /mnt/Torrents 0755 root root -"
   ];
-  fileSystems."/mnt/Data" = {
-    device = "192.168.1.2:/mnt/NAS/Data";
-    fsType = "nfs";
-    options = [
-      "_netdev"
-      "v4"
-      "x-systemd.automount"
-      "x-systemd.mount-timeout=1s"
-      "timeo=14"
-      "retrans=2"
-      "nolock"
-      "soft"
-    ];
-  };
-  fileSystems."/mnt/Torrents" = {
-    device = "192.168.1.2:/mnt/NAS/Torrents";
-    fsType = "nfs";
-    options = [
-      "_netdev"
-      "v4"
-      "x-systemd.automount"
-      "x-systemd.mount-timeout=1s"
-      "timeo=14"
-      "retrans=2"
-      "nolock"
-      "soft"
-    ];
+
+  #==== Montages NFS (NAS) ====
+  fileSystems = {
+    # Partage Data
+    "/mnt/Data" = {
+      device = "192.168.1.2:/mnt/NAS/Data";
+      fsType = "nfs";
+      options = [
+        "_netdev"
+        "v4"
+        "x-systemd.automount"
+        "x-systemd.mount-timeout=1s"
+        "timeo=14"
+        "retrans=2"
+        "nolock"
+        "soft"
+      ];
+    };
+
+    # Partage Torrents
+    "/mnt/Torrents" = {
+      device = "192.168.1.2:/mnt/NAS/Torrents";
+      fsType = "nfs";
+      options = [
+        "_netdev"
+        "v4"
+        "x-systemd.automount"
+        "x-systemd.mount-timeout=1s"
+        "timeo=14"
+        "retrans=2"
+        "nolock"
+        "soft"
+      ];
+    };
   };
 }
