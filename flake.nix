b2c15d123404b3bfa6d1aea4cbf1702e1b2750ee
Sinsry <Sinsry@users.noreply.github.com>
2026-02-01 02:24:38 +0100

    Update

--- flake.nix
+++ flake.nix
@@ -13,8 +13,8 @@
       nixosConfigurations.maousse = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; };
         system = "x86_64-linux";
-        modules = [ ./configuration-gnome.nix ];
-        #modules = [ ./configuration-kde.nix ];
+        #modules = [ ./configuration-gnome.nix ];
+        modules = [ ./configuration-kde.nix ];
       };
     };
 }
