080b190f00aac74bf7aa4ce2b3f989dfda928f9c
Sinsry <Sinsry@users.noreply.github.com>
2026-02-08 01:20:12 +0100

    Update

--- flake.nix
+++ flake.nix
@@ -4,22 +4,16 @@
   #==== Sources ====
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    solaar = {
-      #url = "https://flakehub.com/f/Svenum/Solaar-Flake/*.tar.gz"; # For latest stable version
-      url = "https://flakehub.com/f/Svenum/Solaar-Flake/0.1.7.tar.gz"; # uncomment line for solaar version 1.1.19
-      #url = "github:Svenum/Solaar-Flake/main"; # Uncomment line for latest unstable version
-    };
   };
 
   #==== Configuration ====
   outputs =
-    inputs@{ self, nixpkgs, solaar, ... }:
+    inputs@{ self, nixpkgs, ... }:
     {
       nixosConfigurations.maousse = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; };
         system = "x86_64-linux";
         modules = [
-          solaar.nixosModules.default
           ./configuration-kde.nix
         ];
       };
