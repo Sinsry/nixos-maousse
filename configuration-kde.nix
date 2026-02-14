479e922749a9b67f3b47eb149f11fe85966366b2
Sinsry <Sinsry@users.noreply.github.com>
2026-02-14 06:06:08 +0100

    Update

--- configuration-kde.nix
+++ configuration-kde.nix
@@ -250,16 +250,16 @@
         };
       };
     };
-    vscode = {
-      enable = true;
-      extensions = with pkgs.vscode-extensions; [
-        ms-ceintl.vscode-language-pack-fr
-        anthropic.claude-code
-        jnoortheen.nix-ide
-        ms-vscode-remote.remote-ssh
-        ms-vscode-remote.remote-ssh-edit
-      ];
-    };
+     vscode = {
+       enable = true;
+       extensions = with pkgs.vscode-extensions; [
+         ms-ceintl.vscode-language-pack-fr
+         anthropic.claude-code
+         jnoortheen.nix-ide
+         ms-vscode-remote.remote-ssh
+         ms-vscode-remote.remote-ssh-edit
+       ];
+     };
 
     git = {
       enable = true;
